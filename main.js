aa5e52ca592cc5e42adc0bea42bbbba706c96f41
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2023-12-23 12:12:23 +0800

    RangeError: Invalid position 1 in document of length 0

--- main.js
+++ main.js
@@ -1,45 +1,16 @@
 import { JSONEditor } from "vanilla-jsoneditor";
 
-const content = {
-  json: {
-    bool: false
-  }
-};
-
 const jsonEditor = new JSONEditor({
   target: document.querySelector("#jsonEditor"),
   props: {
-    content,
-    onChange() {
-      console.log('onChange')
-    },
+    mode: 'text',
   }
-});
+})
 
-window.programmaticallyUpdateContentUsingSetAPIByMutatingTheOriginalJSON = () => {
-  content.json.bool = !content.json.bool
-  jsonEditor.set(content);
-};
-
-window.programmaticallyUpdateContentUsingSetAPIByCreatingANewJSON = () => {
-  content.json.bool = !content.json.bool
+window.setJSONValueUnderTextMode = () => {
   jsonEditor.set({
     json: {
-      bool: content.json.bool
+      randomNumber: Math.random()
     }
-  });
-};
-
-window.programmaticallyUpdateContentUsingUpdateAPIByMutatingTheOriginalJSON = () => {
-  content.json.bool = !content.json.bool
-  jsonEditor.update(content);
-};
-
-window.programmaticallyUpdateContentUsingUpdateAPIByCreatingANewJSON = () => {
-  content.json.bool = !content.json.bool
-  jsonEditor.update({
-    json: {
-      bool: content.json.bool
-    }
-  });
-};
+  })
+}
