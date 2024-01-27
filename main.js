8b5c772a59c13c280911f135aa0e598af313a201
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2024-01-27 21:39:10 +0800

    bug: `base` option is not working on `<audio>` & `<video>` tag in `index.html`

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-import { JSONEditor } from "vanilla-jsoneditor";
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
-const jsonEditor = new JSONEditor({
-  target: document.querySelector("#jsonEditor"),
-  props: {
-    mode: 'text',
-  }
-})
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
 
-window.setJSONValueUnderTextMode = () => {
-  jsonEditor.set({
-    json: {
-      randomNumber: Math.random()
-    }
-  })
-}
+setupCounter(document.querySelector('#counter'))
